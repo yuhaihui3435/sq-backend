06ad1fa2deed6ba40a0fb216bb6d31d3c767a8bc
zzq <zhuzz@neuray.cn>
2019-07-24 17:26:56 +0800

    增加发送验证码

--- src/main/java/com/neuray/wp/controller/www/UserController.java
+++ src/main/java/com/neuray/wp/controller/www/UserController.java
@@ -20,6 +20,7 @@ import com.neuray.wp.kits.ReqKit;
 import com.neuray.wp.model.MemberLoginDto;
 import com.neuray.wp.service.RedisCacheService;
 import com.neuray.wp.service.doctor.DoctorService;
+import com.neuray.wp.service.sms.SMSService;
 import com.neuray.wp.service.user.UserInfoService;
 import com.neuray.wp.service.user.UserLoginService;
 import lombok.extern.log4j.Log4j2;
@@ -50,8 +51,7 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("/api/user")
 public class UserController {
 
-    public static final String PREFIX_SMSCODE="SMSCODE_";
-
+    public static final String PREFIX_SMSCODE = "SMSCODE_";
 
 
     @Autowired
@@ -62,38 +62,40 @@ public class UserController {
     private RedisCacheService redisCacheService;
     @Autowired
     private DoctorService doctorService;
+    @Autowired
+    private SMSService smsService;
 
     /**
      * 会员注册
+     *
      * @param userLogin
      * @param smsCode
      * @return
      */
     @PostMapping("/register")
     @ResponseBody
-    public RespBody register(@RequestBody UserLogin userLogin,@RequestParam String smsCode){
-        if(StrUtil.isBlank(userLogin.getPhone())){
+    public RespBody register(@RequestBody UserLogin userLogin, @RequestParam String smsCode) {
+        if (StrUtil.isBlank(userLogin.getPhone())) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("请填写手机号").build();
-        }
-        else if(StrUtil.isBlank(userLogin.getPwd())){
+        } else if (StrUtil.isBlank(userLogin.getPwd())) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("请填写密码").build();
-        }else if(StrUtil.isBlank(smsCode)){
+        } else if (StrUtil.isBlank(smsCode)) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("请填写短信验证码").build();
         }
-        String str=(String)redisCacheService.findVal(PREFIX_SMSCODE+userLogin.getPhone());
-        if(StrUtil.isBlank(str)){
+        String str = (String) redisCacheService.findVal(PREFIX_SMSCODE + userLogin.getPhone());
+        if (StrUtil.isBlank(str)) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("短信验证码过期").build();
-        }else if (!str.equals(smsCode)){
+        } else if (!str.equals(smsCode)) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("短信验证码不正确").build();
         }
 
-        String account=RandomUtil.randomString(9);
-        Boolean bl=true;
-        while(bl) {
+        String account = RandomUtil.randomString(9);
+        Boolean bl = true;
+        while (bl) {
             if (userLoginService.checkAccount(account, null).isEmpty()) {
-                bl=false;
-            }else{
-                account=RandomUtil.randomString(9);
+                bl = false;
+            } else {
+                account = RandomUtil.randomString(9);
             }
         }
         userLogin.setPwd(SecureUtil.hmacMd5(Consts.PWD_SECURE_KEY).digestHex(userLogin.getPwd()));
@@ -106,118 +108,119 @@ public class UserController {
 
     /**
      * 发送短信验证码
+     *
      * @param phone
      * @return
      */
     @GetMapping("/sendSmsCode/{phone}")
     @ResponseBody
-    public RespBody<Object> sendSmsCode(@PathVariable("phone") String phone){
-        if(StrUtil.isBlank(phone)){
+    public RespBody<Object> sendSmsCode(@PathVariable("phone") String phone) {
+        if (StrUtil.isBlank(phone)) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("请填写手机号").build();
         }
         //缓存中检查是否存在验证码，有删掉
-        if(redisCacheService.exist(PREFIX_SMSCODE+phone)){
-            redisCacheService.delVal(PREFIX_SMSCODE+phone);
+        if (redisCacheService.exist(PREFIX_SMSCODE + phone)) {
+            redisCacheService.delVal(PREFIX_SMSCODE + phone);
         }
-        List<UserLogin> userLoginList=userLoginService.checkPhone(phone,null);
-        if(userLoginList.isEmpty()){
+        List<UserLogin> userLoginList = userLoginService.checkPhone(phone, null);
+        if (userLoginList.isEmpty()) {
             //发送短信验证码
-            String code=RandomUtil.randomNumbers(4);
+            String code = RandomUtil.randomNumbers(4);
             //发送验证码
-
+            smsService.sendSms(phone, code);
             //缓存验证码
-            redisCacheService.addVal(PREFIX_SMSCODE+phone,code);
-            redisCacheService.expired(PREFIX_SMSCODE+phone,30, TimeUnit.MINUTES);
+            redisCacheService.addVal(PREFIX_SMSCODE + phone, code);
+            redisCacheService.expired(PREFIX_SMSCODE + phone, 30, TimeUnit.MINUTES);
             return RespBody.builder().code(RespBody.SUCCESS).msg("验证码发送成功").build();
-        }else{
+        } else {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("此手机号已注册").build();
         }
     }
 
     @PostMapping("/login")
     @ResponseBody
-    public RespBody login(@RequestBody MemberLoginDto param, HttpServletRequest request, HttpServletResponse response){
+    public RespBody login(@RequestBody MemberLoginDto param, HttpServletRequest request, HttpServletResponse response) {
 
-        UserLogin userLogin =userLoginService.findByAccountOrTelOrEmail(param.getAccount());
+        UserLogin userLogin = userLoginService.findByAccountOrTelOrEmail(param.getAccount());
         //没有匹配到会员
-        if(userLogin==null){
+        if (userLogin == null) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("账号不存在或已注销").build();
-        //是否被锁定，五分钟内密码联系输错3次，被停用半小时
-        }else if(redisCacheService.exist(Consts.MEMBER_LOCKED+userLogin.getId())){
-            return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("账号被锁定,原因:"+(String) redisCacheService.findVal(Consts.MEMBER_LOCKED+userLogin.getId())).build();
+            //是否被锁定，五分钟内密码联系输错3次，被停用半小时
+        } else if (redisCacheService.exist(Consts.MEMBER_LOCKED + userLogin.getId())) {
+            return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("账号被锁定,原因:" + (String) redisCacheService.findVal(Consts.MEMBER_LOCKED + userLogin.getId())).build();
         }
         //账户状态是否正常
-        else if(userLogin.getStatus().equals(Consts.STATUS.DISABLE.getCode())){
+        else if (userLogin.getStatus().equals(Consts.STATUS.DISABLE.getCode())) {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("账号被停用").build();
         }
         //密码验证失败
-        else if(!userLogin.getPwd().equals(SecureUtil.hmacMd5(Consts.PWD_SECURE_KEY).digestHex(param.getPwd()))){
-            Integer tryCount=(Integer) redisCacheService.findVal(Consts.MEMBER_TRY_COUNT+userLogin.getId());
+        else if (!userLogin.getPwd().equals(SecureUtil.hmacMd5(Consts.PWD_SECURE_KEY).digestHex(param.getPwd()))) {
+            Integer tryCount = (Integer) redisCacheService.findVal(Consts.MEMBER_TRY_COUNT + userLogin.getId());
             //是否已经尝试过，第一次
-            if(tryCount==null) {
-                tryCount=1;
-                redisCacheService.addVal(Consts.MEMBER_TRY_COUNT+userLogin.getId(),tryCount);
+            if (tryCount == null) {
+                tryCount = 1;
+                redisCacheService.addVal(Consts.MEMBER_TRY_COUNT + userLogin.getId(), tryCount);
                 redisCacheService.expired(Consts.MEMBER_TRY_COUNT + userLogin.getId(), 5, TimeUnit.MINUTES);
-            }else{
+            } else {
                 //已经尝试过
-                tryCount=tryCount+1;
-                redisCacheService.addVal(Consts.MEMBER_TRY_COUNT+userLogin.getId(),tryCount);
+                tryCount = tryCount + 1;
+                redisCacheService.addVal(Consts.MEMBER_TRY_COUNT + userLogin.getId(), tryCount);
             }
             //尝试次数达到上限，被锁定
-            if(Consts.MEMBER_LOGIN_TRY_COUNT.equals(tryCount)){
-                redisCacheService.addVal(Consts.MEMBER_LOCKED+userLogin.getId(),"短时间内密码多次错误，账号被锁定半个小时");
-                redisCacheService.expired(Consts.MEMBER_LOCKED+userLogin.getId(),30,TimeUnit.MINUTES);
+            if (Consts.MEMBER_LOGIN_TRY_COUNT.equals(tryCount)) {
+                redisCacheService.addVal(Consts.MEMBER_LOCKED + userLogin.getId(), "短时间内密码多次错误，账号被锁定半个小时");
+                redisCacheService.expired(Consts.MEMBER_LOCKED + userLogin.getId(), 30, TimeUnit.MINUTES);
             }
-            return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("密码错误，还能尝试"+(Consts.MEMBER_LOGIN_TRY_COUNT-tryCount)+"次！").build();
-        }else{
+            return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("密码错误，还能尝试" + (Consts.MEMBER_LOGIN_TRY_COUNT - tryCount) + "次！").build();
+        } else {
             //登录成功，清楚缓存
-            redisCacheService.delVal(Consts.MEMBER_LOCKED+userLogin.getId());
-            redisCacheService.delVal(Consts.MEMBER_TRY_COUNT+userLogin.getId());
-            if(redisCacheService.exist(Consts.CURR_MEMBER+userLogin.getId())) {
-                String str=(String)redisCacheService.findVal(Consts.CURR_MEMBER+userLogin.getId());
+            redisCacheService.delVal(Consts.MEMBER_LOCKED + userLogin.getId());
+            redisCacheService.delVal(Consts.MEMBER_TRY_COUNT + userLogin.getId());
+            if (redisCacheService.exist(Consts.CURR_MEMBER + userLogin.getId())) {
+                String str = (String) redisCacheService.findVal(Consts.CURR_MEMBER + userLogin.getId());
                 redisCacheService.delVal(str);
                 redisCacheService.delVal(Consts.CURR_MEMBER + userLogin.getId());//强制删除当前在线的账户
             }
             //记录最后一次登录时间
-            MemberLoginDto memberLoginDto=new MemberLoginDto();
+            MemberLoginDto memberLoginDto = new MemberLoginDto();
             memberLoginDto.setUserLoginId(userLogin.getId());
             memberLoginDto.setLastLoginAt(new Date());
             memberLoginDto.setLastLoginIp(ReqKit.getRemortIP(request));
             memberLoginDto.setUserLogin(userLogin);
             memberLoginDto.setAccount(userLogin.getAccount());
-            if(Consts.USERLOGIN_TYPE.MEMBER.getCode().equals(userLogin.getType())){
-                UserInfo userInfo=userInfoService.tplOne(UserInfo.builder().loginId(userLogin.getId()).build());
-                if(userInfo!=null){
+            if (Consts.USERLOGIN_TYPE.MEMBER.getCode().equals(userLogin.getType())) {
+                UserInfo userInfo = userInfoService.tplOne(UserInfo.builder().loginId(userLogin.getId()).build());
+                if (userInfo != null) {
                     memberLoginDto.setUserInfo(userInfo);
                     memberLoginDto.setMemberType(Consts.USERLOGIN_TYPE.MEMBER.getCode());
                     memberLoginDto.setMemberInfoComplete(true);
                 }
 
-            }else if(Consts.USERLOGIN_TYPE.DOCTOR.getCode().equals(userLogin.getType())){
-                Doctor doctor=doctorService.tplOne(Doctor.builder().loginId(userLogin.getId()).build());
-                if(doctor!=null){
+            } else if (Consts.USERLOGIN_TYPE.DOCTOR.getCode().equals(userLogin.getType())) {
+                Doctor doctor = doctorService.tplOne(Doctor.builder().loginId(userLogin.getId()).build());
+                if (doctor != null) {
                     memberLoginDto.setDoctor(doctor);
                     memberLoginDto.setMemberType(Consts.USERLOGIN_TYPE.DOCTOR.getCode());
                     memberLoginDto.setMemberInfoComplete(true);
                 }
             }
 
-            Map<String,Object> map=new HashMap<>();
-            map.put("memberLoginInfo",memberLoginDto);
+            Map<String, Object> map = new HashMap<>();
+            map.put("memberLoginInfo", memberLoginDto);
 
             String token = JwtKit.createJWT(UUID.fastUUID().toString(), map, -1L);
-            redisCacheService.addVal(Consts.CURR_MEMBER+userLogin.getId(),token);
-            redisCacheService.addVal(token,memberLoginDto);
+            redisCacheService.addVal(Consts.CURR_MEMBER + userLogin.getId(), token);
+            redisCacheService.addVal(token, memberLoginDto);
 
             /**
              * 记住我，可免登陆7天
              */
-            if(param.getRememberMe()){
-                redisCacheService.expired(Consts.CURR_MEMBER+userLogin.getId(),7,TimeUnit.DAYS);
-                redisCacheService.expired(token,7,TimeUnit.DAYS);
-            }else{
-                redisCacheService.expired(Consts.CURR_MEMBER+userLogin.getId(),8,TimeUnit.HOURS);
-                redisCacheService.expired(token,8,TimeUnit.HOURS);
+            if (param.getRememberMe()) {
+                redisCacheService.expired(Consts.CURR_MEMBER + userLogin.getId(), 7, TimeUnit.DAYS);
+                redisCacheService.expired(token, 7, TimeUnit.DAYS);
+            } else {
+                redisCacheService.expired(Consts.CURR_MEMBER + userLogin.getId(), 8, TimeUnit.HOURS);
+                redisCacheService.expired(token, 8, TimeUnit.HOURS);
             }
 
             return RespBody.builder().code(RespBody.SUCCESS).msg("登录成功").body(token).build();
@@ -227,18 +230,19 @@ public class UserController {
 
     /**
      * 退出系统
+     *
      * @param request
      * @return
      */
     @PostMapping("/logout")
     @ResponseBody
-    public RespBody logout(HttpServletRequest request){
-        String token=CookieKit.getUid(request,Consts.MEMBER_TOKEN);
-        if(StrUtil.isNotBlank(token)) {
+    public RespBody logout(HttpServletRequest request) {
+        String token = CookieKit.getUid(request, Consts.MEMBER_TOKEN);
+        if (StrUtil.isNotBlank(token)) {
             MemberLoginDto memberLoginDto = (MemberLoginDto) redisCacheService.findVal(token);
             redisCacheService.delVal(token);
-            if(memberLoginDto!=null){
-                redisCacheService.delVal(Consts.CURR_MEMBER+memberLoginDto.getUserLoginId());
+            if (memberLoginDto != null) {
+                redisCacheService.delVal(Consts.CURR_MEMBER + memberLoginDto.getUserLoginId());
             }
         }
         return RespBody.builder().code(RespBody.SUCCESS).msg("退出成功").build();
@@ -246,21 +250,22 @@ public class UserController {
 
     /**
      * 修改密码
+     *
      * @param param
      * @param request
      * @return
      */
     @PostMapping("/modifyPwd")
     @ResponseBody
-    public RespBody modifyPwd(@RequestBody MemberLoginDto param,HttpServletRequest request){
-        String token=CookieKit.getUid(request,Consts.MEMBER_TOKEN);
-        MemberLoginDto memberLoginDto=(MemberLoginDto)redisCacheService.findVal(token);
-        UserLogin userLogin=userLoginService.one(memberLoginDto.getUserLoginId());
-        String pwd=userLogin.getPwd();
-        if(SecureUtil.hmacMd5(Consts.PWD_SECURE_KEY).digestHex(param.getPwd()).equals(pwd)){
+    public RespBody modifyPwd(@RequestBody MemberLoginDto param, HttpServletRequest request) {
+        String token = CookieKit.getUid(request, Consts.MEMBER_TOKEN);
+        MemberLoginDto memberLoginDto = (MemberLoginDto) redisCacheService.findVal(token);
+        UserLogin userLogin = userLoginService.one(memberLoginDto.getUserLoginId());
+        String pwd = userLogin.getPwd();
+        if (SecureUtil.hmacMd5(Consts.PWD_SECURE_KEY).digestHex(param.getPwd()).equals(pwd)) {
             userLogin.setPwd(SecureUtil.hmacMd5(Consts.PWD_SECURE_KEY).digestHex(param.getNewPwd()));
             userLoginService.update(userLogin);
-        }else{
+        } else {
             return RespBody.builder().code(RespBody.BUSINESS_ERROR).msg("原始密码不正确").build();
         }
         return RespBody.builder().code(RespBody.SUCCESS).msg("密码修改成功").build();
@@ -269,30 +274,32 @@ public class UserController {
 
     /**
      * 会员详细信息更新
+     *
      * @param userInfo
      * @return
      */
     @PostMapping("/modifyUserInfo")
     @ResponseBody
-    public RespBody modifyUserInfo(@RequestBody UserInfo userInfo){
+    public RespBody modifyUserInfo(@RequestBody UserInfo userInfo) {
         userInfoService.update(userInfo);
         return RespBody.builder().build().success("详细信息更新成功");
     }
 
     /**
      * 医生信息更新
+     *
      * @param doctor
      * @return
      */
     @PostMapping("/modifyDoctorInfo")
     @ResponseBody
-    public RespBody modifyUserInfo(@RequestBody Doctor doctor){
+    public RespBody modifyUserInfo(@RequestBody Doctor doctor) {
         doctorService.update(doctor);
         return RespBody.builder().build().success("详细信息更新成功");
     }
 
     @PostMapping("/info")
-    public UserInfo getUserInfo(@RequestParam String userId){
+    public UserInfo getUserInfo(@RequestParam String userId) {
         return userInfoService.one(userId);
     }
 
---
+++ src/main/java/com/neuray/wp/service/sms/SMSService.java
@@ -0,0 +1,52 @@
+package com.neuray.wp.service.sms;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @ClassName SMSService
+ * @Description TODO
+ * @Author zzq
+ * @Date 2019/3/30 15:01
+ * @Version 1.0
+ **/
+@Service
+@Transactional
+public class SMSService {
+
+    public boolean sendSms(String phone, String content) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI3y85qrY7TS1G", "LHW4ARfQ5OZ3l5Ptwm6j6XzyTgHiBl");
+        IAcsClient client = new DefaultAcsClient(profile);
+        CommonRequest request = new CommonRequest();
+        //request.setProtocol(ProtocolType.HTTPS);
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", phone);
+//        request.putQueryParameter("SignName", "教思乐平台");
+        request.putQueryParameter("TemplateCode", "SMS_169660540");
+        request.putQueryParameter("TemplateParam", "{\"code\":\"" + content + "\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+            return true;
+        } catch (ServerException e) {
+            e.printStackTrace();
+            return false;
+        } catch (ClientException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
